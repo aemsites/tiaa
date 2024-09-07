8b2d6682b2f3ce6884ce9fcf31d741742abbc0f9
Andrei Tuicu <tuicu@adobe.com>
2024-09-07 15:52:14 +0200

    poc - hero

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,6 +1,49 @@
-import { div } from "../../scripts/dom-helpers.js";
+import { toCamelCase } from '../../scripts/aem.js';
+import { div } from '../../scripts/dom-helpers.js';
+
+function readHeroBlockConfig(block) {
+  const config = {};
+
+  block.querySelectorAll(':scope > div').forEach((row) => {
+    if (row.children) {
+      const cols = [...row.children];
+      if (cols[1]) {
+        const col = cols[1];
+        const name = toCamelCase(cols[0].textContent);
+        config[name] = col.children || [];
+      }
+    }
+  });
+  return config;
+}
 
 export default function decorate(block) {
+  const heroConfig = readHeroBlockConfig(block);
+  let mobileImage = heroConfig.mobileImage ? heroConfig.mobileImage[0] : null;
+  let desktopImage = heroConfig.desktopImage ? heroConfig.desktopImage[0] : null;
+  const image = heroConfig.image ? heroConfig.image[0] : null;
+
+  if (mobileImage && desktopImage) {
+    mobileImage.classList.add('mobile-image');
+    desktopImage.classList.add('desktop-image');
+  } else {
+    mobileImage = mobileImage || desktopImage || image;
+    desktopImage = desktopImage || mobileImage || image;
+  }
+
+  block.innerHTML = '';
+  block.appendChild(
+    div({ class: 'grid' },
+      div({ class: 'hero-text' },
+        ...heroConfig.text,
+      ),
+      div({ class: 'hero-image' },
+        mobileImage, // mobile first always for LHS
+        desktopImage,
+      ),
+    ),
+  );
+
   const heroText = block.firstElementChild.firstElementChild;
   const heroImage = block.firstElementChild.lastElementChild;
 
@@ -19,12 +62,16 @@ export default function decorate(block) {
     heroImage.classList.add(...'g-col-md-5 g-col-sm-5 g-col-12'.split(' '));
   }
 
+  let heroGroupLayout = 'g-col-md-10 g-col-12 offset-md-2';
+  if (block.classList.contains('pathing-hero')) {
+    heroGroupLayout = 'g-col-md-10 g-col-12 offset-md-2 offset-sm-2';
+  }
   heroText.appendChild(
-    div({ class: 'hero-group grid'},
-      div({ class: 'g-col-md-10 g-col-12 offset-md-2 offset-sm-2' },
-        ...block.querySelectorAll("p")
+    div({ class: 'hero-group grid' },
+      div({ class: heroGroupLayout },
+        ...block.querySelectorAll('p'),
       ),
-    )
+    ),
   );
 
   heroText.querySelector('p').classList.add('body-1');
