955788f17e27d17a9250d5463495e3f94ca8ca2c
Andrei Tuicu <tuicu@adobe.com>
2024-09-09 15:39:39 +0200

    poc - minimal styles + hero

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -34,7 +34,7 @@ async function loadFonts() {
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
  */
-function buildAutoBlocks(main) {
+function buildAutoBlocks(/* main */) {
   try {
     // buildHeroBlock(main);
   } catch (error) {
