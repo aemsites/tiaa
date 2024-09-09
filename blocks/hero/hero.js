4a58746f885b0a60a2c9d0ec1e7da582d98d9988
Andrei Tuicu <tuicu@adobe.com>
2024-09-09 21:32:52 +0200

    test - dynamic media

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,6 +1,64 @@
 import { toCamelCase } from '../../scripts/aem.js';
 import { div } from '../../scripts/dom-helpers.js';
 
+function dynamicMediaOptimisedPicture(src, component, alt, eager) {
+  const url = new URL(src, window.location.href);
+  url.search = '';
+  if (url.pathname.includes(':')) {
+    url.pathname = url.pathname.split(':')[0];
+  }
+  console.log(url);
+
+  const picture = document.createElement('picture');
+  const formats = [
+    { type: 'image/webp', format: 'webp' },
+    { type: 'image/jpeg', format: 'webp&amp;pscan=5' },
+  ]
+
+  const pixels = [
+    { suffix: '', pixels: 1 },
+    { suffix: '-Retina-2x', pixels: 2 },
+    { suffix: '-Retina-3x', pixels: 3 },
+  ];
+
+  const breakpoints = [ 
+    { 
+      media: "(max-width: 599px)",
+      size: 'ExtraSmall',
+    },
+    { 
+      media: "(max-width: 959px)",
+      size: 'Small',
+    },
+    { media: "(max-width: 1279px)",
+      size: 'Medium',
+    },
+    { media: "(max-width: 1440px)",
+      size: 'Large',
+    },
+    { size: 'ExtraLarge' },
+  ];
+
+  formats.forEach((format) => {
+    breakpoints.forEach((br) => {
+      const source = document.createElement('source');
+      if (br.media) source.setAttribute('media', br.media);
+      source.setAttribute('type', format.type);
+      source.setAttribute('srcset', pixels.map((pixel) => `${url}:${component}-${br.size}${pixel.suffix}?fmt=${format.format} ${pixel.pixels}x`).join(','));
+      picture.appendChild(source);
+    });
+  });
+
+  const img = document.createElement('img');
+  img.setAttribute('loading', eager ? 'eager' : 'lazy');
+  img.setAttribute('alt', alt || true);
+  img.setAttribute('src', url.href);
+
+  picture.appendChild(img);
+  
+  return picture;
+}
+
 function readHeroBlockConfig(block) {
   const config = {};
 
@@ -44,16 +102,24 @@ export default function decorate(block) {
     ),
   );
 
-  block.querySelector('.hero-image').innerHTML = `
-    <picture>
-      <source media="(max-width: 599px)"  srcset="https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-ExtraSmall?ts=1710250504138&amp;fmt=webp&amp;pscan=5 1x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-ExtraSmall-Retina-2x?ts=1710250504138&amp;fmt=webp&amp;pscan=5 2x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-ExtraSmall-Retina-3x?ts=1710250504138&amp;fmt=webp&amp;pscan=5 3x">
-      <source media="(max-width: 959px)"  srcset="https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Small?ts=1710250504138&amp;fmt=webp&amp;pscan=5 1x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Small-Retina-2x?ts=1710250504138&amp;fmt=webp&amp;pscan=5 2x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Small-Retina-3x?ts=1710250504138&amp;fmt=webp&amp;pscan=5 3x">
-      <source media="(max-width: 1279px)" srcset="https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Medium?ts=1710250504138&amp;fmt=webp&amp;pscan=5 1x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Medium-Retina-2x?ts=1710250504138&amp;fmt=webp&amp;pscan=5 2x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Medium-Retina-3x?ts=1710250504138&amp;fmt=webp&amp;pscan=5 3x">
-      <source media="(max-width: 1440px)" srcset="https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Large?ts=1710250504139&amp;fmt=webp&amp;pscan=5 1x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Large-Retina-2x?ts=1710250504139&amp;fmt=webp&amp;pscan=5 2x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Large-Retina-3x?ts=1710250504139&amp;fmt=webp&amp;pscan=5 3x">
-      <source                             srcset="https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-ExtraLarge?ts=1710250504139&amp;fmt=webp&amp;pscan=5 1x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-ExtraLarge-Retina-2x?ts=1710250504139&amp;fmt=webp&amp;pscan=5 2x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-ExtraLarge-Retina-3x?ts=1710250504139&amp;fmt=webp&amp;pscan=5 3x">
-      <img alt="" fetchpriority="high" title="Hero-home.jpg" class="img-center img-top" src="https://s7d1.scene7.com/is/image/tiaastage/home-page-hero">
-    </picture>
-  `
+  block.querySelector('.hero-image').appendChild(
+    dynamicMediaOptimisedPicture(
+      'https://s7d1.scene7.com/is/image/tiaastage/home-page-hero',
+      'Hero-With-Children',
+      'test',
+    ),
+  );
+
+  // block.querySelector('.hero-image').innerHTML = `
+  //   <picture>
+  //     <source media="(max-width: 599px)"  srcset="https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-ExtraSmall?ts=1710250504138&amp;fmt=webp&amp;pscan=5 1x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-ExtraSmall-Retina-2x?ts=1710250504138&amp;fmt=webp&amp;pscan=5 2x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-ExtraSmall-Retina-3x?ts=1710250504138&amp;fmt=webp&amp;pscan=5 3x">
+  //     <source media="(max-width: 959px)"  srcset="https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Small?ts=1710250504138&amp;fmt=webp&amp;pscan=5 1x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Small-Retina-2x?ts=1710250504138&amp;fmt=webp&amp;pscan=5 2x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Small-Retina-3x?ts=1710250504138&amp;fmt=webp&amp;pscan=5 3x">
+  //     <source media="(max-width: 1279px)" srcset="https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Medium?ts=1710250504138&amp;fmt=webp&amp;pscan=5 1x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Medium-Retina-2x?ts=1710250504138&amp;fmt=webp&amp;pscan=5 2x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Medium-Retina-3x?ts=1710250504138&amp;fmt=webp&amp;pscan=5 3x">
+  //     <source media="(max-width: 1440px)" srcset="https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Large?ts=1710250504139&amp;fmt=webp&amp;pscan=5 1x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Large-Retina-2x?ts=1710250504139&amp;fmt=webp&amp;pscan=5 2x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-Large-Retina-3x?ts=1710250504139&amp;fmt=webp&amp;pscan=5 3x">
+  //     <source                             srcset="https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-ExtraLarge?ts=1710250504139&amp;fmt=webp&amp;pscan=5 1x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-ExtraLarge-Retina-2x?ts=1710250504139&amp;fmt=webp&amp;pscan=5 2x,https://s7d1.scene7.com/is/image/tiaastage/home-page-hero:Hero-With-Children-ExtraLarge-Retina-3x?ts=1710250504139&amp;fmt=webp&amp;pscan=5 3x">
+  //     <img alt="" fetchpriority="high" title="Hero-home.jpg" class="img-center img-top" src="https://s7d1.scene7.com/is/image/tiaastage/home-page-hero">
+  //   </picture>
+  // `
 
   const heroText = block.querySelector('.hero-text');
   const heroImage = block.querySelector('.hero-image');
