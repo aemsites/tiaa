bef1fdde5ce14a15d7817b480ceda3e52b62e465
Peter A <52969899+rootpea@users.noreply.github.com>
2024-09-24 02:00:29 +0200

    11-card-stack: add stack card and stack component blocks

--- blocks/stackcard/stackcard.js
+++ blocks/stackcard/stackcard.js
@@ -4,21 +4,21 @@ function decorateStackCardContent(div) {
     // first child
     const eyeBrow = content.children[0];
     if (eyeBrow) {
-      eyeBrow.classList.add("qui-stack-card-eyebrow");
-      eyeBrow.classList.add("eyebrow-1");
-      eyeBrow.classList.add("color-accent-tertiary");
+      eyeBrow.classList.add('qui-stack-card-eyebrow');
+      eyeBrow.classList.add('eyebrow-1');
+      eyeBrow.classList.add('color-accent-tertiary');
     }
     const title = content.children[1];
     if (title) {
-      title.classList.add("qui-stack-card-title");
-      title.classList.add("jumbo-1");
-      title.classList.add("color-accent-secondary");
-      title.classList.add("stat-title");
+      title.classList.add('qui-stack-card-title');
+      title.classList.add('jumbo-1');
+      title.classList.add('color-accent-secondary');
+      title.classList.add('stat-title');
     }
     const description = content.children[2];
     if (description) {
-      description.classList.add("qui-stack-card-description");
-      description.classList.add("body-1");
+      description.classList.add('qui-stack-card-description');
+      description.classList.add('body-1');
     }
   }
 }
@@ -28,18 +28,18 @@ export default function decorate(block) {
   block.classList.add(`stackcard-${rows.length}-rows`);
 
   rows.forEach((row, index) => {
-    row.classList.add("stackcard-qui-stack-card");
-    row.classList.add("stackcard-row");
+    row.classList.add('stackcard-qui-stack-card');
+    row.classList.add('stackcard-row');
     row.classList.add(`stackcard-row-${index + 1}`);
     const columns = [...row.children];
     columns.forEach((column) => {
       // create a wrapper div and move the column inside it
-      const wrapper = document.createElement("div");
-      wrapper.classList.add("stackcard-wrapper");
-      wrapper.classList.add("stackcard-qui-enhanced-card");
+      const wrapper = document.createElement('div');
+      wrapper.classList.add('stackcard-wrapper');
+      wrapper.classList.add('stackcard-qui-enhanced-card');
       column.parentNode.insertBefore(wrapper, column);
       wrapper.appendChild(column);
-      column.classList.add("stackcard-content");
+      column.classList.add('stackcard-content');
       decorateStackCardContent(column);
     });
   });
--- blocks/stackcomponent/stackcomponent.js
+++ blocks/stackcomponent/stackcomponent.js
@@ -4,18 +4,18 @@ function decoratestackcomponentContent(div) {
     // first child
     const eyeBrow = content.children[0];
     if (eyeBrow) {
-      eyeBrow.classList.add("qui-stack-component-eyebrow");
-      eyeBrow.classList.add("eyebrow-2");
-      eyeBrow.classList.add("color-accent-tertiary");
+      eyeBrow.classList.add('qui-stack-component-eyebrow');
+      eyeBrow.classList.add('eyebrow-2');
+      eyeBrow.classList.add('color-accent-tertiary');
     }
     const title = content.children[1];
     if (title) {
-      title.classList.add("qui-stack-component-title");
+      title.classList.add('qui-stack-component-title');
     }
     const description = content.children[2];
     if (description) {
-      description.classList.add("qui-stack-component-description");
-      description.classList.add("body-1");
+      description.classList.add('qui-stack-component-description');
+      description.classList.add('body-1');
     }
   }
 }
@@ -25,18 +25,18 @@ export default function decorate(block) {
   block.classList.add(`stackcomponent-${rows.length}-rows`);
 
   rows.forEach((row, index) => {
-    row.classList.add("stackcomponent-qui-stack-component");
-    row.classList.add("stackcomponent-row");
+    row.classList.add('stackcomponent-qui-stack-component');
+    row.classList.add('stackcomponent-row');
     row.classList.add(`stackcomponent-row-${index + 1}`);
     const columns = [...row.children];
     columns.forEach((column) => {
       // create a wrapper div and move the column inside it
-      const wrapper = document.createElement("div");
-      wrapper.classList.add("stackcomponent-wrapper");
-      wrapper.classList.add("stackcomponent-qui-enhanced-card");
+      const wrapper = document.createElement('div');
+      wrapper.classList.add('stackcomponent-wrapper');
+      wrapper.classList.add('stackcomponent-qui-enhanced-card');
       column.parentNode.insertBefore(wrapper, column);
       wrapper.appendChild(column);
-      column.classList.add("stackcomponent-content");
+      column.classList.add('stackcomponent-content');
       decoratestackcomponentContent(column);
     });
   });
