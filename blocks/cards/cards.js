8067541a8227d67cb9929fc8347d63e827f67440
cmota-adobe-com <cmota@adobe.com>
2024-09-27 14:58:48 +0300

    Implemented custom cards for personalized advice and count on us sections

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -5,14 +5,32 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    // li.classList.add('ethos-2-theme')
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('img')) {
+        div.className = 'card-image';
+      } else {
+        div.className = 'card-container';
+      }
+      div.querySelectorAll('h2,h3,h4,h5')
+        .forEach((e) => {
+          e.classList.add('card-title', 'heading-5');
+        });
+      div.querySelectorAll('p')
+        .forEach((e) => {
+          if (e.classList.contains('button-container')) {
+            e.classList.add('card-button');
+          } else {
+            e.classList.add('card-text');
+          }
+        });
     });
     ul.append(li);
   });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  ul.querySelectorAll('picture > img')
+    .forEach((img) => img.closest('picture')
+      .replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
 }
